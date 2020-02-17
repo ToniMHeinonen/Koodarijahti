966392e596277164082ab24662fcf7c563b3fcdf
Toni <toni.heinonen@hotmail.fi>
2020-02-17 20:36:58 +0200

    Add style to <p> score element.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+
+const MyP = styled('p')({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  width: 200,
+  padding: '0 30px',
+});
 
 class App extends React.Component {
 
   constructor() {
     super()
-    this.state = {'score': 0}
+    this.state = {'score': ''}
     this.fetchData = this.fetchData.bind(this)
   }
 
@@ -14,13 +26,13 @@ class App extends React.Component {
     console.log('fetchData')
     const data = await fetch('http://localhost:8080/increment').then(data => data.json())
     const str = `You received ${data.pointsWon} points,
-                clicks needed for next win ${data.neededPressesForWin}`
+                clicks needed for next win: ${data.neededPressesForWin}`
     this.setState({'score': str})
   }
   
   render() {
     const btn = <Button variant="contained" onClick={this.fetchData}>Click me</Button>
-    const score = <p>{this.state.score}</p>
+    const score = <MyP>{this.state.score}</MyP>
     return (<div>{btn}{score}</div>)
   }
 }
