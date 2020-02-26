2c196f66fd95336faa4f729b213d9be14443cfe6
Toni <toni.heinonen@hotmail.fi>
2020-02-26 15:49:23 +0200

    Rename 'Project' folder to 'backend'.

--- Project/README.md
+++
@@ -1,2 +0,0 @@
-# Koodarijahti
- Job exercise
--- Project/src/main/java/io/github/tonimheinonen/koodarijahti/CounterRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.tonimheinonen.koodarijahti;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CounterRepository extends CrudRepository<Counter, Integer> {
-
-}
