a6a8497bfcb230317e6bdad44190c5c59f10d551
Toni <toni.heinonen@hotmail.fi>
2020-02-26 14:50:01 +0200

    Move custom styles to MyStyles from App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import {HeaderBg, BottomBg} from './MyStyles.js'
 
-// Stylize background for text
-const MyP = styled('p')({
-  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-  border: 0,
-  borderRadius: 3,
-  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  color: 'white',
-  height: 48,
-  width: 200,
-  padding: '0 30px',
-});
 // const URL = 'https://shrouded-fjord-28724.herokuapp.com/'  // Heroku backend
 const URL = 'http://localhost:8080' // Local backend
 const gameOverText = 'GAME OVER'
@@ -102,16 +92,25 @@ class App extends React.Component {
     let score, btn, receivedPoints
 
     if (!this.gameOver) {
-      score = <MyP>Your score: {this.curPoints}</MyP>
+      score = <HeaderBg>Your score: {this.curPoints}</HeaderBg>
       btn = <Button variant="contained" onClick={this.fetchData}>Click me</Button>
     } else {
-      score = <MyP>{this.headerText}</MyP>
+      score = <HeaderBg>{this.headerText}</HeaderBg>
       btn = <Button variant="contained" onClick={this.startGame}>Start Game</Button>
     }
 
-    receivedPoints = <MyP>{this.state.receivedPoints}</MyP>
+    receivedPoints = <BottomBg>{this.state.receivedPoints}</BottomBg>
     
-    return (<div>{score}{btn}{receivedPoints}</div>)
+    return (<div>
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+              >
+                {score}{btn}{receivedPoints}
+              </Grid>
+            </div>)
   }
 }
 
---
+++ frontend/src/MyStyles.js
@@ -0,0 +1,26 @@
+import { styled } from '@material-ui/core/styles';
+
+const boxHeight = 48;
+
+const TextBgParent = styled('p')({
+  background: 'linear-gradient(360deg, #000 10%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 10,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  width: 200,
+  textAlign: 'center',
+  fontSize: '2em',
+  fontFamily: 'Times New Roman',
+  padding: '0 30px'
+});
+
+const HeaderBg = styled(TextBgParent)({
+  height: boxHeight
+});
+
+const BottomBg = styled(TextBgParent)({
+  height: (boxHeight*2)
+});
+
+export {HeaderBg, BottomBg}
